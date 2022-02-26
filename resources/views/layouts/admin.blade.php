7f76b0036ef18023aff01079dd57c84be4a9132b
ThoBT <thob2105@gmail.com>
2022-02-26 16:34:12 +0700

    setup themes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,24 +1,10 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Tai Duc Window</title>
-
-        {{-- bootstrap css --}}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-              integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-              crossorigin="anonymous">
-        {{-- fonts --}}
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
-        {{-- customs css --}}
-        <link rel="stylesheet" href="{{asset('assets/admin/css/main.css')}}">
+        @include('admin.head')
     </head>
-    <body>
+    <body class="hold-transition">
+
         {{-- header --}}
         @include('admin.blocks.header')
 
@@ -28,10 +14,6 @@
         {{-- footer --}}
         @include('admin.blocks.footer')
 
-
-        {{-- bootstrap script --}}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-                crossorigin="anonymous"></script>
+        @include('admin.foot')
     </body>
 </html>
