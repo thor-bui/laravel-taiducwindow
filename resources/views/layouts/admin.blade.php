1a187039611f856fc65d5e1675fc731d3eea17bc
ThoBT <thob2105@gmail.com>
2022-02-27 12:20:05 +0700

    remove footer

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,10 +10,10 @@
 
             <aside class="main-sidebar sidebar-dark-primary elevation-4">
                 <!-- Brand Logo -->
-                <a href="../../index3.html" class="brand-link">
+                <a href="#" class="brand-link">
                     <img src="{{asset('assets/admin/template/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
                          class="brand-image img-circle elevation-3" style="opacity: .8">
-                    <span class="brand-text font-weight-light">AdminLTE 3</span>
+                    <span class="brand-text font-weight-light">AdminTDW</span>
                 </a>
 
                 <!-- Sidebar -->
@@ -32,9 +32,6 @@
                 <!-- /.content -->
             </div>
             <!-- /.content-wrapper -->
-
-            {{-- footer --}}
-            @include('admin.blocks.footer')
 
             @include('admin.foot')
         </div>
